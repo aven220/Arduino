cb301e47ec47bd4e3c8c38467d65367e023f475a
Anderson Avendaño <90359826+aven220@users.noreply.github.com>
2023-04-14 00:02:47 -0500

    Add files via upload

---
+++ token.h
@@ -0,0 +1,5 @@
+const char* ssid = "Anderson";
+const char* password = "12345678";
+const String token = "6164593600:AAGoq1uDOntMjHAS6c0nemigFhoWojBiVZc"; 
+const int IDchat = 1359970870;
+const String nombre = "Casa";
